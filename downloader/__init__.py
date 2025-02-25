a3987d84a45b79ee89946cfb7fa706bd933b0e1b
Mumao0526 <apple338824@gmail.com>
2025-02-26 02:44:08 +0800

    Fix when Running AudioSegment.from_wav or audio.export will pop up the cmd command window.

--- downloader/__init__.py
+++ downloader/__init__.py
@@ -3,3 +3,19 @@
 
 # logger = logging.getLogger(__name__)
 # logger.addHandler(logging.NullHandler())
+import subprocess
+
+
+class NoWindowPopen(subprocess.Popen):
+    def __init__(self, *args, **kwargs):
+        startupinfo = subprocess.STARTUPINFO()
+        startupinfo.dwFlags = (
+            subprocess.CREATE_NEW_CONSOLE | subprocess.STARTF_USESHOWWINDOW
+        )
+        startupinfo.wShowWindow = subprocess.SW_HIDE
+
+        kwargs["startupinfo"] = startupinfo
+        super().__init__(*args, **kwargs)
+
+
+subprocess.Popen = NoWindowPopen
